eb742488432f50e538a8edc49d1f00dc5576da97
Facundo Manresa <facundo.manresa@claro.com.ar>
2022-07-28 09:09:47 -0300

    Refactorización de código.

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -1,11 +1,27 @@
+import { useState } from "react";
 import "./ItemCount.css";
 
-const ItemCount = ({ onAdd, onRemove, counter }) => {
+const ItemCount = () => {
+  const [counter, setCounter] = useState(0);
+  let stockTotal = 5;
+  let initialStock = 0;
+  const onAdd = () => {
+    if (counter < stockTotal) setCounter(counter + 1);
+  };
+
+  const onRemove = () => {
+    if (counter > initialStock) setCounter(counter - 1);
+  };
+
   return (
-    <div>
-      <button onClick={onRemove}>-</button>
-      <button>{counter}</button>
-      <button onClick={onAdd}>+</button>
+    <div className="counterContainer">
+      <button className="counterContainer__btn" onClick={onRemove}>
+        -
+      </button>
+      <p className="counterContainer__num">{counter}</p>
+      <button className="counterContainer__btn" onClick={onAdd}>
+        +
+      </button>
     </div>
   );
 };
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,23 +1,10 @@
 import ItemCount from "../ItemCount/ItemCount";
-import { useState } from "react";
 
 const ItemListContainer = ({ title }) => {
-  const [counter, setCounter] = useState(0);
-  let stockTotal = 5;
-  let initialStock = 0;
-
-  const onAdd = () => {
-    if (counter < stockTotal) setCounter(counter + 1);
-  };
-
-  const onRemove = () => {
-    if (counter > initialStock) setCounter(counter - 1);
-  };
-
   return (
     <>
       <h1>{title}</h1>
-      <ItemCount onAdd={onAdd} onRemove={onRemove} counter={counter} />
+      <ItemCount />
     </>
   );
 };
